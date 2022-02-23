62933f336ff31c1030a8a0256e4bd8de44386856
Rucciva <rucciva@gmail.com>
2022-02-23 15:08:46 +0700

    added firewall and print cert when no key

--- examples/01-minimum.tfvars
+++ examples/01-minimum.tfvars
@@ -17,6 +17,15 @@ nebula_mesh = {
     {
       name = "node1"
       ip   = "192.168.127.2/24"
+    },
+    {
+      name       = "phone1"
+      ip         = "192.168.127.3/24"
+      public_key = <<-EOF
+        -----BEGIN NEBULA X25519 PUBLIC KEY-----
+        1f/iyVqtpEXsBSvvihF6MPHbEqXMsy0+bfWurXtu9HY=
+        -----END NEBULA X25519 PUBLIC KEY-----
+      EOF
     }
   ]
 }
--- variables.tf
+++ variables.tf
@@ -29,13 +29,33 @@ variable "nebula_mesh" {
       am_lighthouse = optional(bool)
       blocked       = optional(bool)
       listen = optional(object({
-        host = string
-        port = number
+        host = optional(string)
+        port = optional(number)
       }))
       addresses = optional(list(object({
         host = string
         port = optional(number)
       })))
+      firewall = optional(object({
+        inbound = optional(list(object({
+          port    = string
+          proto   = string
+          host    = optional(string)
+          ca_name = optional(string)
+          group   = optional(string)
+          groups  = optional(list(string))
+          cidr    = optional(string)
+        })))
+        outbound = optional(list(object({
+          port    = string
+          proto   = string
+          ca_name = optional(string)
+          host    = optional(string)
+          group   = optional(string)
+          groups  = optional(list(string))
+          cidr    = optional(string)
+        })))
+      }))
     }))
   })
   description = "Membership data of nebula network"
@@ -43,6 +63,29 @@ variable "nebula_mesh" {
   validation {
     condition     = length(var.nebula_mesh.ca.instance_ids) > 0 && join("|", var.nebula_mesh.ca.instance_ids) == join("|", distinct(compact(var.nebula_mesh.ca.instance_ids)))
     error_message = "The `ca.instance_ids` must contains at least 1 item. It should contains arbritrary unique string that will be associated to each generated CA. The public certificate of all CAs will be added to `pki.ca` configuration object, but only CA referenced by the first ID will be used to sign certificates for all nodes."
-
+  }
+  validation {
+    condition     = length(var.nebula_mesh.nodes) == length(distinct([for nodes in var.nebula_mesh.nodes : split("/", nodes.ip)[0]]))
+    error_message = "The `nodes[*].ip` should be unique on each node."
+  }
+  validation {
+    condition = length([
+      for rule in flatten([
+        for node in var.nebula_mesh.nodes :
+        concat(
+          lookup(node.firewall, "inbound", null) == null ? [] : node.firewall.inbound,
+          lookup(node.firewall, "outbound", null) == null ? [] : node.firewall.outbound
+        )
+        if lookup(node, "firewall", null) != null
+      ]) :
+      rule
+      if lookup(rule, "ca_name", null) == null &&
+      lookup(rule, "host", null) == null &&
+      lookup(rule, "group", null) == null &&
+      lookup(rule, "cidr", null) == null &&
+      length(lookup(rule, "groups", [])) == 0
+      ]
+    ) == 0
+    error_message = "Invalid firewall definition. at least one of `host`, `group`, `groups`, `cidr`, or `ca_name` must be provided."
   }
 }
